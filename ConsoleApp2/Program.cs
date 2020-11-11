9f6c591cafea608fdcedd4857654bc9243b38cd4
Viktor <krpectv57@gmail.com>
2020-11-11 21:00:31 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BOOK LOTR = new BOOK();
+            LOTR.SetBook("idiot", 1);
+        }
+    }
+    class BOOK
+    {
+        public string title;
+        private int pages;
+        public void SetBook(string a, int b)
+        {
+            title = a;
+            pages = b;
+
+            Console.WriteLine(title + " " + pages);
+        }
+    }
+}
